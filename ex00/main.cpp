3b13a3a4e767169066574db87bbb061a8b488e43
CharlesLeChauve <teddy.gibert@gmail.com>
2024-09-26 11:16:17 +0200

    LINUX compiles

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,14 @@
 #include "BitcoinExchange.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "Usage: ./btc <input_file>" << std::endl;
+		return (1);
+	}
 	BitcoinExchange be("data.csv");
 
-	be.treatInput("input.txt");
+	be.treatInput(av[1]);
+	return 0;
 }
